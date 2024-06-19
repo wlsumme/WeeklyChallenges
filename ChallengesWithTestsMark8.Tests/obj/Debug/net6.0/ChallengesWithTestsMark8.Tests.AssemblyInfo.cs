9a1e639d5397b70a55aa6be39d0364567ffa83bf
William Summe <wlsumme@gmail.com>
2024-06-19 11:15:00 -0500

    done

--- ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2572a50c2e0f96558f54c9350cd29ff3601455e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a05ba4799597dc7594243f236530c9d1cd14e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -6,47 +6,186 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var sum = 0;
+
+            foreach(int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+                else
+                {
+                    sum -= number;
+                }
+            }
+                return sum;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+
+
+
+            var minLength = str1.Length;
+
+            if(minLength > str2.Length)
+            {
+                minLength = str2.Length;
+            }
+            if(minLength > str3.Length)
+            {
+                minLength = str3.Length;
+            }
+            if (minLength > str4.Length)
+            {
+                minLength = str4.Length;
+            }
+            return minLength;
+            
+            
+            
+            //return Math.Min(Math.Min(str1.Length, str2.Length), Math.Min(str3.Length, str4.Length));
+            //    if(str1.Length < str2.Length && str1.Length < str3.Length && str1.Length < str4.Length)
+            //    {
+            //        return str1.Length;
+            //    }
+            //    else if (str2.Length < str1.Length && str2.Length < str3.Length && str2.Length < str4.Length)
+            //    {
+            //        return str2.Length;
+            //    }
+            //    else if (str3.Length < str1.Length && str3.Length < str2.Length && str3.Length < str4.Length)
+            //    {
+            //        return str3.Length;
+            //    }
+            //    else
+            //    {
+            //        return str4.Length;
+            //    }
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var smallestNum = number1;
+            if(smallestNum > number2)
+            {
+                smallestNum = number2;
+            } 
+            if(smallestNum > number3)
+            {
+                smallestNum = number3;
+            }
+            if (smallestNum > number4)
+            {
+                smallestNum = number4;
+            }
+            return smallestNum;
+            
+            
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if(sideLength1 + sideLength2 > sideLength3 && 
+                sideLength2 + sideLength3 > sideLength1 &&
+                sideLength1 + sideLength3 > sideLength3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            if(input == null)
+            {
+                return false;
+            }
+            return double.TryParse(input, out _);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            if(objs == null || objs.Length == 0)
+            {
+                return false;
+            }
+            var nullCount = 0;
+            
+            foreach(var obj in objs)
+            {
+                if(obj == null)
+                {
+                    nullCount++;
+                }
+
+            }
+            var elem = objs.Length / 2;
+            if(nullCount > elem)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            double evenAmount = 0;
+            double evenNumber = 0;
+
+            foreach(var number in numbers)
+            {
+                if(number % 2 == 0)
+                {
+                    evenAmount += number;
+                    evenNumber++;
+
+                }
+
+            }
+            if(evenAmount == 0)
+            {
+                return 0;
+            }
+            return evenAmount / evenNumber;
+
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0)
+            {
+                return 1;
+            }
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Factorial is not defined for negative numbers.");
+
+            }
+
+            var fact = 1;
+
+            for(var i = 1; i<= number; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+
         }
     }
 }
